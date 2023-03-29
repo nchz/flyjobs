aaa50749338c2e9200b22d166b946b24b57a9bf6
Ignacio Gonzalez <8524584+nchz@users.noreply.github.com>
2023-03-28 22:22:51 -0300

    add Swagger.

--- src/flyjobs/urls.py
+++ src/flyjobs/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from core.views import JobViewSet, UserViewSet
 
@@ -9,7 +11,20 @@ router = DefaultRouter()
 router.register("jobs", JobViewSet, "job")
 router.register("users", UserViewSet, "user")
 
+schema_view = get_schema_view(
+    title="Flyjobs",
+    description="API details.",
+)
+
+swagger_view = TemplateView.as_view(
+    template_name="swagger-ui.html",
+    extra_context={"schema_url": "openapi-schema"},
+)
+
+
 urlpatterns = [
+    path("", swagger_view, name="swagger-ui"),
+    path("schema/", schema_view, name="openapi-schema"),
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
